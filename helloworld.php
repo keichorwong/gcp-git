3e0ca2d59ab86c5a4141e9c59dddbe3875a5fc56
Ken Wong <wongkeichor@gmail.com>
2014-11-03 16:45:22 +0800

    Changed app.yaml and hellowrld.php

--- helloworld.php
+++ helloworld.php
@@ -3,7 +3,7 @@ use google\appengine\api\users\User;
 use google\appengine\api\users\UserService;
 $user = UserService::getCurrentUser();
 		if ($user) {
-			echo "Hello, " . htmlspecialchars($user->getNickname());
+			echo "Hello buddy, " . htmlspecialchars($user->getNickname());
 		}
 		else {
 			header('Location: ' . UserService::createLoginURL($_SERVER['REQUEST_URI']));
